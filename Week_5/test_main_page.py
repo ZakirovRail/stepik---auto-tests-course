252332581f065f5cf1f244f055043216d5c09dd8
R F <R@gmail.com>
2020-07-13 11:23:00 +0200

    Week 5 - 5.4 added test should_be_login_link modified BasePage class - added is_element_presented method to catch exceptions

--- Week_5/test_main_page.py
+++ Week_5/test_main_page.py
@@ -9,6 +9,14 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
 
 
+def test_guest_should_see_login_link(browser):
+    link = "https://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
 if __name__ == '____main__':
     test_guest_can_go_to_login_page()
 
