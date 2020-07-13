18b79e4bb71a9647dd97dd48d35423727d0ebb3d
R F <R@gmail.com>
2020-07-13 11:06:07 +0200

    Week 5 - 5.4 added methods to the main_page file and rewritten the test test_guest_can_go_to_login_page

--- Week_5/test_main_page.py
+++ Week_5/test_main_page.py
@@ -1,8 +1,12 @@
+from .pages.main_page import MainPage
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "https://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
 
 
 if __name__ == '____main__':
