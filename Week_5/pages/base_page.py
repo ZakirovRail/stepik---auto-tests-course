6efb6d87feacc6fa87251781517d541e383b22c2
R F <R@gmail.com>
2020-07-15 08:43:21 +0200

    Week 5 - added 2 tests:   test_guest_should_see_login_link_on_product_page() test_guest_can_go_to_login_page_from_product_page()

--- Week_5/pages/base_page.py
+++ Week_5/pages/base_page.py
@@ -1,6 +1,10 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import math
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
+from .locators import LoginPageLocators
 
 
 class BasePage():
@@ -32,3 +36,23 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Registration form is not presented"
--- Week_5/pages/product_page.py
+++ Week_5/pages/product_page.py
@@ -1,36 +1,54 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
-from .locators import GoodsPageLocators
+from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
 
     def should_be_add_button(self):
         assert self.is_element_present(
-            *GoodsPageLocators.ADD_TO_BASKET_BUTTON), "The 'Add to basket' button is not presented"
+            *ProductPageLocators.ADD_TO_BASKET_BUTTON), "The 'Add to basket' button is not presented"
 
     def should_be_book_name(self):
-        assert self.is_element_present(*GoodsPageLocators.BOOK_NAME), "The book name is not presented"
+        assert self.is_element_present(*ProductPageLocators.BOOK_NAME), "The book name is not presented"
 
     def should_be_book_price(self):
-        assert self.is_element_present(*GoodsPageLocators.BOOK_PRICE), "The price for the book is not presented"
+        assert self.is_element_present(*ProductPageLocators.BOOK_PRICE), "The price for the book is not presented"
         # вопрос как возвращать полученное значение и потом принимать его как входной параметр в следующей проверке?
 
     def add_good_to_basket(self):
-        add_button = self.browser.find_element(*GoodsPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         add_button.click()
 
     def should_be_info_message_about_adding(self):
-        # info_message = self.browser.find_element(*GoodsPageLocators.ABOUT_ADDING_MESSAGE)
+        # info_message = self.browser.find_element(*ProductPageLocators.ABOUT_ADDING_MESSAGE)
         assert self.is_element_present(
-            *GoodsPageLocators.ABOUT_ADDING_MESSAGE), "No message about successful adding of a book to the asket"
+            *ProductPageLocators.ABOUT_ADDING_MESSAGE), "No message about successful adding of a book to the asket"
 
     def should_be_equal_book_name(self):
-        book_name = self.browser.find_element(*GoodsPageLocators.BOOK_NAME).text
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
         assert book_name == self.browser.find_element(
-            *GoodsPageLocators.ADDED_BOOK_NAME).text, "The name of added book is not equal to expected name"
+            *ProductPageLocators.ADDED_BOOK_NAME).text, "The name of added book is not equal to expected name"
 
     def should_be_equal_book_price(self):
-        book_price = self.browser.find_element(*GoodsPageLocators.BOOK_PRICE).text
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
         assert book_price == self.browser.find_element(
-            *GoodsPageLocators.ADDED_BOOK_PRICE).text, "The total price in the basket is not equal as a price for the " \
-                                                       "added book"
+            *ProductPageLocators.ADDED_BOOK_PRICE).text, "The total price in the basket is not equal as a price for the " \
+                                                         "added book"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located(*ProductPageLocators.ADDED_BOOK_NAME))
+        except TimeoutException:
+            return False
+        return True
+
+    def should_not_be_element(self):
+        assert BasePage.is_not_element_present(*ProductPageLocators.ADDED_BOOK_NAME), "Login link is not presented"
